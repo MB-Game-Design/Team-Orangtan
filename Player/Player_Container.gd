c95f89003ed35c27d2275fa7721d6effd0482d83
Jason Francis <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-12-05 17:47:49 -0500

    Does that help?
    Let me know what else you are hoping to do.

--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -2,18 +2,15 @@ extends Node2D
 
 @onready var Player = load("res://Player/Player.tscn")
 var starting_position = Vector2(200,100)
-
-
-func _ready():
-	pass
-
+var died = false
 
 func _physics_process(_delta):
-	if not has_node("Player"):
+	if get_child_count() == 0:
+		if died:
+			var ball_audio = get_node_or_null("/root/Game/Ball_Audio")
+			if ball_audio != null:
+				ball_audio.play()
 		var player = Player.instantiate()
 		player.position = starting_position
 		add_child(player)
-		if get_child_count() == 0:
-		var ball_audio = get_node_or_null("/root/Game/Ball_Audio")
-		if ball_audio != null:
-			ball_audio.play()
+		died = true
