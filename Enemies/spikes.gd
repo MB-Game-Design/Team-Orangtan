6d3d7681a11aa3717411865095f3f233221eb1de
Jason Francis <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-12-04 21:40:45 -0500

    Cleans up a few things
    I fixed the assets and the scripts for the spikes, angry spikes, saw, and the water. Let me know if I can help with anything else.

--- Enemies/spikes.gd
+++ Enemies/spikes.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body):
-	if is a Node and body.get_path().as_text() == "res://Player/Player.tscn":
-		queue_free()
+	if body.name == "Player":
+		body.queue_free()
